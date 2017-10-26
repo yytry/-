0b9e4f121b6d94ef11c847f58c4d3f471e92f3a7
yytry <15925604638@163.com>
2017-10-26 19:40:41 +0800

    message

---
+++ Online_Message.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<meta charset=utf-8/>
+<head>
+    <style type="text/css">
+        h1 {letter-spacing:30px}
+    </style>
+    <title>留言板from果冻。</title>
+    <style>body{background-image:url(timg.jpg);
+        background-size:cover;
+        background-repeat:no-repeat;
+        background-position:center center;
+        overflow: hidden;
+        height:100%;
+        background-attachment:fixed;
+        position:relative;}
+        form{position:absolute;left:600px;top:80px;}
+        h1{position:absolute;left:800px;}
+    </style>
+</head>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="name";
+// 创建连接
+$conn = new mysqli($servername, $username, $password,$dbname);
+// 检测连接
+$conn->query("set name 'utf8'");
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);}
+?>
+<form action="up.php" method="POST">
+    <h4 style="background: transparent;"> 你是我的谁？</h4><br>
+    <input  type="text" width="200px" height="30px" name="name" style="background: transparent;" >
+    <br>
+    <h4 style="background: transparent;"> 想对我说什么？</h4><br>
+    <textarea name="document" id="0" cols="40" rows="10" style="background: transparent;" ></textarea>
+<br>
+    <input type="submit" value="留言" name="1" style="background: transparent;height:30px;width:50px"><br>
+    <script type="text/javascript">
+        function btnclick() {$.ajax({type: "get"  url: "up.php" success: "alert(msg)"});}
+    </script>
+    <body>
+    <h1>留 言 板</h1>
+
+</form>
+</body>
+</html>
+
+
+
+
---
+++ up.php
@@ -0,0 +1,39 @@
+﻿<?php
+$con = new mysqli("localhost","root","", "name");
+$sql="insert into online(name,document) values ('{$_POST['name']}','{$_POST['document']}')";
+echo '留言成功!',"<br>";
+$result = $con->query($sql);
+print_r($result);
+$sql1="SELECT name,document FROM online";
+$result1=mysqli_query($con,$sql1);
+if (mysqli_num_rows($result1) > 0) {
+    // 输出数据
+    while ($row = mysqli_fetch_assoc($result1)) {
+        echo "我是".$row["name"] . "<br>";
+        echo "想对你说：" .$row["document"] . "<br>";
+    }
+}else {echo "无结果";}
+mysqli_close($con);
+?>
+<html>
+<body>
+<style>body{
+        background-image:url(MC.jpg);};
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-position:center center;
+    overflow: hidden;
+    height:100%;
+    background-attachment:fixed;
+    position:relative;}
+    form{position:absolute; left:600px; top:80px; } h1{position:absolute; left:800px;}
+</style>
+<form  action="Online Message.php" method="POST">
+<input type="submit" value="继续留言" style="background: transparent;height:40px;width:70px"><br>
+    <script type="text/javascript">
+    function btnclick(){$.ajax({type: "get"  url: "Online Message.php"  success:  alert(msg)"});}
+    </script>
+</form>
+</body>
+</html>
+
